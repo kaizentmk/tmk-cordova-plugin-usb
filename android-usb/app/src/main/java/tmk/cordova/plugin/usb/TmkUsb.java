bf0c03a9e73f1ea04a36ba65a234d2ac375b059a
tmk <kaizentmk@gmail.com>
2020-01-20 20:22:15 +0100

    Tmkcordovapluginusb

--- android-usb/app/src/main/java/tmk/cordova/plugin/usb/TmkUsb.java
+++ android-usb/app/src/main/java/tmk/cordova/plugin/usb/TmkUsb.java
@@ -4,28 +4,60 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 public class TmkUsb extends CordovaPlugin {
 
     private int count = 0;
 
     @Override
-    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+    public boolean execute(String action,
+                           JSONArray data,
+                           CallbackContext callbackContext)
+            throws JSONException {
 
         count++;
-
-        if (action.equals("listUsbDevices")) {
-
-            String name = data.getString(0);
-            String message = count + "Hello, " + name;
-            callbackContext.success(message);
-
-            return true;
-
-        } else {
-
+        try {
+            switch (action) {
+                case "greet":
+                    return greet(data, callbackContext);
+                case "info":
+                    return info(data, callbackContext);
+                case "listUsbDevices":
+                    return listDevices(data, callbackContext);
+                default:
+                    callbackContext.error("Unsupported action: " + action);
+                    return false;
+            }
+        } catch (Throwable t) {
+            callbackContext.error(t.getMessage());
             return false;
-
         }
+
+    }
+
+
+    private boolean greet(JSONArray data, CallbackContext callbackContext) throws JSONException {
+        String name = data.getString(0);
+        String message = count + " - Hello, " + name;
+        callbackContext.success(message);
+
+        return true;
+    }
+
+    private boolean info(JSONArray data, CallbackContext callbackContext)
+            throws JSONException {
+        JSONObject jo = new JSONObject();
+        jo.put("serviceName", this.getServiceName());
+        jo.put("hasPermisssion", this.hasPermisssion());
+        callbackContext.success(jo);
+        return true;
+    }
+
+
+    private boolean listDevices(JSONArray data, CallbackContext callbackContext) {
+        JSONArray ja = new JSONArray();
+        callbackContext.success(ja);
+        return true;
     }
 }
--- www/tmk-usb.js
+++ www/tmk-usb.js
@@ -1,7 +1,45 @@
 /*global cordova, module*/
 
+/**
+ * cordova.exec(function(winParam) {},
+             function(error) {},
+             "service",
+             "action",
+             ["firstArgument", "secondArgument", 42, false]);
+
+             Here is how each parameter works:
+
+function(winParam) {}: A success callback function. Assuming your exec call completes successfully, this function executes along with any parameters you pass to it.
+
+function(error) {}: An error callback function. If the operation does not complete successfully, this function executes with an optional error parameter.
+
+"service": The service name to call on the native side. This corresponds to a native class, for which more information is available in the native guides listed below.
+
+"action": The action name to call on the native side. This generally corresponds to the native class method. See the native guides listed below.
+
+[ arguments ]: An array of arguments to pass into the native environment.
+*/
+
 module.exports = {
     greet: function (name, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "TmkUsb", "listUsbDevices", [name]);
+        cordova.exec(successCallback,
+            errorCallback,
+            "TmkUsb",
+            "greet",
+            [name]);
+    },
+    info: function (successCallback, errorCallback) {
+        cordova.exec(successCallback,
+            errorCallback,
+            "TmkUsb",
+            "info",
+            []);
+    },
+    listUsbDevices: function (successCallback, errorCallback) {
+        cordova.exec(successCallback,
+            errorCallback,
+            "TmkUsb",
+            "listUsbDevices",
+            []);
     }
 };
